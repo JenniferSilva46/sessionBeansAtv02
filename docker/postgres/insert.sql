ba651753c899f7fa3b20bec80933915642fe5154
JenniferSilva48 <jennifer.silva@academico.ifpb.edu.br>
2022-03-17 21:52:32 -0300

    Adicionando estrutura do banco via docker

---
+++ docker/postgres/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO CLIENTE (nome, cpf) VALUES ('Jennifer','22222222222');
+INSERT INTO CLIENTE (nome, cpf) VALUES ('Jose','33333333333');
+INSERT INTO CLIENTE (nome, cpf) VALUES ('Maria','44444444444');
+
+
+INSERT INTO PRODUTO (descricao, valor) VALUES ('Celular','3300');
+INSERT INTO PRODUTO (descricao, valor) VALUES ('Calça','300');
+INSERT INTO PRODUTO (descricao, valor) VALUES ('Panela','240');
+
+
+INSERT INTO VENDA (produto, cliente) VALUES (1, 1);
+INSERT INTO VENDA (produto, cliente) VALUES (2, 2);
+INSERT INTO VENDA (produto, cliente) VALUES (3, 3);
